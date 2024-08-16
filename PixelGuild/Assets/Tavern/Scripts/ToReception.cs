e84c11cb8a19b4a5b9b08d2e1819d34ed60086b1
Razumenz <pasha.razumnov.05@mail.ru>
2024-08-16 07:59:22 +0300

    То что сделала эля и пару кнопок
    Загрузил то что не получилось у элен и добавил пару кнопок для переходов

---
+++ PixelGuild/Assets/Tavern/Scripts/ToReception.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneReception : MonoBehaviour
+{
+
+
+
+    public void LoadScene()
+    {
+
+        SceneManager.LoadScene("Reception");
+    }
+}
