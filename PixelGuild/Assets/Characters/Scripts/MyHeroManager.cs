fe8f59f091827a3ce446f0b9f0149f0d29bb017a
Razumenz <pasha.razumnov.05@mail.ru>
2024-08-30 19:07:34 +0300

    откат
    откат

---
+++ PixelGuild/Assets/Characters/Scripts/MyHeroManager.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyHeroManager : MonoBehaviour
+{
+    public Transform heroListContainer; // Контейнер для списка героев
+    public GameObject heroListItemPrefab; // Префаб элемента списка героя
+    public List<HeroData> allHeroes = new List<HeroData>(); // Список всех доступных героев
+
+    void Start()
+    {
+        PopulateHeroList();
+    }
+
+    void PopulateHeroList()
+    {
+        foreach (HeroData heroData in allHeroes)
+        {
+            // Создаем новый элемент списка для каждого героя
+            GameObject newHeroItem = Instantiate(heroListItemPrefab, heroListContainer);
+            newHeroItem.transform.Find("HeroNameText").GetComponent<Text>().text = heroData.Name;
+            newHeroItem.transform.Find("HeroRankText").GetComponent<Text>().text = heroData.rank;
+            newHeroItem.transform.Find("HeroEnergyText").GetComponent<Text>().text = "Energy: " + heroData.energy;
+            newHeroItem.transform.Find("HeroLevelText").GetComponent<Text>().text = "Level: " + heroData.level;
+        }
+    }
+}
